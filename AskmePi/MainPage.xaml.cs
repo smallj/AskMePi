a23c9f1c6bdc190195835e94822b3c150319b86d
Jim Small <smallj@live.com>
2019-01-13 17:06:10 -0500

    Initial checkin 2

---
+++ AskmePi/MainPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Cognitive.LUIS;
+using RaspberryModules.App.Modules;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.SpeechRecognition;
+using Windows.Media.SpeechSynthesis;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Core;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AskmePi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private SpeechRecognizer _contSpeechRecognizer;
+        private readonly string _luiskey = "67c6110ee4784feb8491e71894319525";
+        private readonly string _appId = "08d65b73-860f-4ee6-bc45-5e65143874bb";
+        private readonly RGBLed _rgbLed = new RGBLed();
+        private readonly SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
+        private readonly MediaPlayer _speechPlayer = new MediaPlayer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _rgbLed.Init();
+        }
+        
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _contSpeechRecognizer = new SpeechRecognizer();
+            await _contSpeechRecognizer.CompileConstraintsAsync();
+            _contSpeechRecognizer.ContinuousRecognitionSession.ResultGenerated += ContinuousRecognitionSession_ResultGenerated;
+            _contSpeechRecognizer.ContinuousRecognitionSession.Completed += ContinuousRecognitionSession_Completed;
+            await _contSpeechRecognizer.ContinuousRecognitionSession.StartAsync();
+            var voice = SpeechSynthesizer.AllVoices.FirstOrDefault(i => i.Gender == VoiceGender.Female) ?? SpeechSynthesizer.DefaultVoice;
+            _synthesizer.Voice = voice;
+        }
+        
+        private async void ContinuousRecognitionSession_Completed(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionCompletedEventArgs args)
+        {
+            Debug.WriteLine($"Completed > Restart listening");
+            await _contSpeechRecognizer.ContinuousRecognitionSession.StartAsync();
+        }
+        
+        private async void ContinuousRecognitionSession_ResultGenerated(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            string speechResult = args.Result.Text;
+
+            //To have the app respond to a keyword only put the call to handle result to this if statement
+            //if (speechResult.ToLower().StartsWith("hey"))
+            //{
+            //    // CODE HERE
+            //}
+
+            Debug.WriteLine($"Text: {speechResult}");
+
+            LuisClient client = new LuisClient(_appId, _luiskey);
+            LuisResult result = await client.Predict(speechResult);
+
+            Debug.WriteLine($"LUIS Result: {result.Intents.First().Name} {string.Join(",", result.Entities.Select(a => $"{a.Key}:{a.Value.First().Value}"))}");
+            HandleLuisResult(result);
+        }
+
+        public void HandleLuisResult(LuisResult result)
+        {
+            if (!result.Intents.Any())
+            {
+                return;
+            }
+
+            switch (result.Intents.First().Name)
+            {
+                case "ControlLED":
+
+                    if (result.Entities.Any(a => a.Key == "LedState"))
+                    {
+                        string ledState = result.Entities.First(a => a.Key == "LedState").Value.First().Value;
+
+                        if (ledState == "on")
+                        {
+                            if (result.Entities.Any(a => a.Key == "LedColor"))
+                            {
+                                string ledColor = result.Entities.First(a => a.Key == "LedColor").Value.First().Value;
+
+                                SayAsync($"Turning on the {ledColor} light.");
+
+                                switch (ledColor)
+                                {
+                                    case "red":
+                                        _rgbLed.TurnOnLed(LedStatus.Red);
+                                        break;
+
+                                    case "green":
+                                        _rgbLed.TurnOnLed(LedStatus.Green);
+                                        break;
+
+                                    case "blue":
+                                        _rgbLed.TurnOnLed(LedStatus.Blue);
+                                        break;
+
+                                    case "purple":
+                                        _rgbLed.TurnOnLed(LedStatus.Purple);
+                                        break;
+                                    default:
+                                        _rgbLed.TurnOnLed(LedStatus.White);
+                                        break;
+                                }
+                            }
+                            else
+                                _rgbLed.TurnOnLed(LedStatus.White);
+                        }
+                        else if (ledState == "off")
+                        {
+                            SayAsync($"Turning off the light.");
+                            _rgbLed.TurnOffLed();
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public async Task SayAsync(string text)
+        {
+            using (var stream = await _synthesizer.SynthesizeTextToStreamAsync(text))
+            {
+                _speechPlayer.Source = MediaSource.CreateFromStream(stream, stream.ContentType);
+            }
+            _speechPlayer.Play();
+        }
+    }
+}
+
---
+++ AskmePi/RGBModule.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Devices.Gpio;
+
+namespace RaspberryModules.App.Modules
+{
+    public enum LedStatus { Red, Green, Blue, Purple, White };
+
+    public class RGBLed
+    {
+        private GpioPin _bluepin;
+
+        private GpioPin _redpin;
+
+        private GpioPin _greenpin;
+
+        public bool IsEnabled;
+
+        public void Init()
+        {
+            try
+            {
+                // Init the LED
+                var gpio = GpioController.GetDefault();
+
+                // Blue
+                _bluepin = gpio.OpenPin(13);
+                if (_bluepin != null)
+                {
+                    _bluepin.Write(GpioPinValue.High);
+                    _bluepin.SetDriveMode(GpioPinDriveMode.Output);
+                }
+
+                // Green
+                _greenpin = gpio.OpenPin(6);
+                if (_greenpin != null)
+                {
+                    _greenpin.Write(GpioPinValue.High);
+                    _greenpin.SetDriveMode(GpioPinDriveMode.Output);
+                }
+
+                // Red
+                _redpin = gpio.OpenPin(5);
+                if (_redpin != null)
+                {
+                    _redpin.Write(GpioPinValue.High);
+                    _redpin.SetDriveMode(GpioPinDriveMode.Output);
+                }
+
+                IsEnabled = true;
+                TurnOffLed();
+            }
+            catch (Exception e)
+            {
+                IsEnabled = false;
+                Debug.WriteLine($"LED: Unable to init. {e.Message}");
+            }
+        }
+
+        public void TurnOnLed(LedStatus ledStatus)
+        {
+            if (IsEnabled)
+            {
+                switch (ledStatus)
+                {
+                    case LedStatus.Red:
+                        _redpin.Write(GpioPinValue.Low);
+                        _bluepin.Write(GpioPinValue.High);
+                        _greenpin.Write(GpioPinValue.High);
+                        break;
+
+                    case LedStatus.Green:
+                        _redpin.Write(GpioPinValue.High);
+                        _greenpin.Write(GpioPinValue.Low);
+                        _bluepin.Write(GpioPinValue.High);
+                        break;
+
+                    case LedStatus.Blue:
+                        _redpin.Write(GpioPinValue.High);
+                        _greenpin.Write(GpioPinValue.High);
+                        _bluepin.Write(GpioPinValue.Low);
+                        break;
+
+                    case LedStatus.Purple:
+                        _redpin.Write(GpioPinValue.Low);
+                        _greenpin.Write(GpioPinValue.High);
+                        _bluepin.Write(GpioPinValue.Low);
+                        break;
+
+                    case LedStatus.White:
+                        _redpin.Write(GpioPinValue.Low);
+                        _greenpin.Write(GpioPinValue.Low);
+                        _bluepin.Write(GpioPinValue.Low);
+                        break;
+
+                }
+            }
+            else
+            {
+                Debug.WriteLine($"LED {ledStatus} ON (RGB Led not found)");
+            }
+        }
+
+        public void TurnOffLed()
+        {
+            if (IsEnabled)
+            {
+                _redpin.Write(GpioPinValue.High);
+                _greenpin.Write(GpioPinValue.High);
+                _bluepin.Write(GpioPinValue.High);
+
+            }
+            else
+            {
+                Debug.WriteLine("LED OFF (RGB Led not found)");
+            }
+        }
+    }
+}
